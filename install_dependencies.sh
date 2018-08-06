28cca52d2099161e9d3372d57424e3ad2c154007
tricher00 <tricher00@gmail.com>
2018-08-06 07:49:11 -0400

    Added install_dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get install python3-numpy
+sudo apt-get install libblas-dev
+sudo apt-get install liblapack-dev
+sudo apt-get install python3-dev # Possibly already installed
+sudo apt-get install libatlas-base-dev # Optional
+sudo apt-get install gfortran
+sudo apt-get install python3-setuptools # Possibly already installed
+sudo apt-get install python3-scipy
+sudo pip3 install keras
+pip3 install -U tensorflow
+pip3 install pandas
